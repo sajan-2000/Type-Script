909d047d22bf86843c5829928207be1098fe3286
Sajan Kumar Swain <sajankumarswain1@gmail.com>
2024-07-02 19:12:53 +0530

    annotation-types

---
+++ Type_annotation.ts
@@ -0,0 +1,14 @@
+//Definition
+//A way of explicitly specifying the type of variable ,function parameter and function return value.
+
+//Advantages
+//It helps ts compiler to enforce type checking and provide type safety during developement.
+
+let myNum: number = 10;
+let myStr: string = "sajan";
+let pi: number = 3.141;
+let myNegVal: number = -2;
+
+
+
+// myStr = 20; //Error 
