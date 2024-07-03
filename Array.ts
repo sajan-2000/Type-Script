999abd6419bb2363574ecd59af3faf03ca88a1a5
Sajan Kumar Swain <sajankumarswain1@gmail.com>
2024-07-03 16:48:26 +0530

    array in ts

--- Array.ts
+++ Array.ts
@@ -8,3 +8,15 @@ let numArr2: number[] = new Array(1, 2, 3, 4, 5);
 
 //* Using Array.of()
 let strArr: string[] = Array.of("sajan", "salman", "don");
+
+//how to use readonly
+//The readonly keyword can prevent arrays from being changed
+
+let nameStr: readonly string[] = ["sajan"];
+
+// nameStr.push("hello");   // it will throw error
+
+//!Tuple
+//A tuple is a typed array with a pre-defined length and types for each index.
+
+let tupleArr: [string, boolean, number] = ["sajan", true, 5];
