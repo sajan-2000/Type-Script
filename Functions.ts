1bff161f584cf4ea9fec9b9b7595d1727644d100
Sajan Kumar Swain <sajankumarswain1@gmail.com>
2024-07-03 11:20:55 +0530

    Functions in ts

---
+++ Functions.ts
@@ -0,0 +1,11 @@
+// function greet(name: string, id: number): string {
+//     return `Hii ${name} Welcome your id is ${id}`;
+// }
+
+// console.log(greet("Sajan", 5));
+
+const greet = (name: string, id: number): string => {
+    return `Hii ${name} Welcome your id is ${id}`;
+}
+
+console.log(greet("Sajan", 5));
