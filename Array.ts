158d86284dc3d3739bc508b4c3cce9aa705f22a0
Sajan Kumar Swain <sajankumarswain1@gmail.com>
2024-07-03 11:29:44 +0530

    Array in ts

---
+++ Array.ts
@@ -0,0 +1,10 @@
+//How to initialize an array
+
+//* Using Square brackets
+let numArr: number[] = [1, 2, 3, 4, 5];
+
+//* Using Array Constructor
+let numArr2: number[] = new Array(1, 2, 3, 4, 5);
+
+//* Using Array.of()
+let strArr: string[] = Array.of("sajan", "salman", "don");
